d7a1708706cf86b6270705b60d4b63f81c9b0b09
Dale Fugier <dale@mcneel.com>
2013-09-26 10:32:57 -0700

    Initial commit.

---
+++ SampleCsSkinObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SampleCsSkin
+{
+  public class SampleCsSkinObject : Rhino.Runtime.Skin
+  {
+    protected override string ApplicationName
+    {
+      get
+      {
+        return "SampleCsSkin";
+      }
+    }
+
+    protected override System.Drawing.Bitmap MainRhinoIcon
+    {
+      get
+      {
+        Icon icon = new Icon(Properties.Resources.SampleCsSkin, new Size(32, 32));
+        return icon.ToBitmap();
+      }
+    }
+  }
+}
